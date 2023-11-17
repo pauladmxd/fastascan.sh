55cd0441ec0681cca5c5e701183eee8b7934ecb4
paula <pauladelgadomanzano@MBP-de-Paula.home>
2023-11-17 19:20:18 +0100

    Mensaje descriptivo de los cambios

--- fastascan_paula.sh
+++ fastascan_paula.sh
@@ -1,29 +1,34 @@
 
 #Folder x where to search
-if [[ -n $1 ]];
-then
+if [[ -n $1 ]]; then
+
     fasta_files=$(find $1 \( -type l -o -type f \) \( -name "*.fa" -or -name "*.fasta" \) );
     folder=$1;
+    
 else
     fasta_files=$(find . \( -type l -o -type f \) \( -name "*.fa" -or -name "*.fasta" \)  );
     folder="the current folder";
 fi;
 
-#echo $fasta_files;
+echo -e "\nWelcome to FastaScan by Paula Delgado\n";
+
+echo -e "------------Summary folder report-----------\n"
 
 echo "Your desired folder is" $folder
 
 #Number of lines
-if [[ -n $2 ]];
-then
+
+if [[ -n $2 ]];then
+
 	nlines=$2;
 else
 	nlines=0;
- fi;
+fi;
  
-#How many files there are
+#How many files are in the folder
 
-echo "Number of fasta files:" $(echo "$fasta_files" | wc -l);
+number_files=$(echo "$fasta_files" | wc -l)
+echo "### Number of fasta files:$number_files";
 
 #Unique IDs
 
@@ -34,15 +39,39 @@ for file in $fasta_files; do
     total_unique_ID=$((unique_ID + total_unique_ID))
 done;
 
-echo "Unique identifiers:" $total_unique_ID
+echo "### Total Unique ID: $total_unique_ID" ;
 
 #For each file
+echo -e "\n------------Single file report-----------\n"
 
 for file in $fasta_files; do
-   if [[ $file -s ]]; then
 
+	file_name=$(basename "$file");
 
+	echo -e "### File name: $file_name\n";
 
-   else 
-		 echo The file $file is empty;
-fi; done;
+	if [[ -s $file ]]; then # empty or not
+
+		if [[ -h $file ]]; then #Symlink or not
+		
+			echo "> It is a symlink that links to" $(readlink $file)
+	
+		else
+		
+			echo "> It is not a link"
+		
+		fi;
+		
+		number_sequences=$(grep -c ">" $file); #number of sequences found in the file
+	
+		echo "> Number of sequences: $number_sequences";
+	
+	else
+	
+		echo "> The file is empty"
+	
+	fi;
+	
+	echo -e "\n"; # empty line
+	
+done;
